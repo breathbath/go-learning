542bac6a1ac1c001291d7999c58dc9abffd910d0
Andrey P <posandrey@gmail.com>
2019-05-06 17:24:38 +0200

    Added data race use case

--- main.go
+++ main.go
@@ -5,31 +5,32 @@ import (
 	error2 "github.com/breathbath/go-learning/error"
 	"github.com/breathbath/go_utils/utils/env"
 	"github.com/breathbath/go_utils/utils/io"
+	"sync"
 )
 
 var persons = []Person{
 	{
-		name: "Andrey",
+		name:    "Andrey",
 		ageRate: 220,
 	},
 	{
-		name: "Roman",
+		name:    "Roman",
 		ageRate: 230,
 	},
 	{
-		name: "Ahmed",
+		name:    "Ahmed",
 		ageRate: 240,
 	},
 	{
-		name: "",
+		name:    "",
 		ageRate: 250,
 	},
 }
 
-var dbIsDown = true
+var dbIsDown = false
 
 type Person struct {
-	name string
+	name    string
 	ageRate int
 }
 
@@ -39,7 +40,7 @@ func main() {
 	if err != nil {
 		switch e := err.(type) {
 		case error2.WarningError:
-			io.OutputWarning( "", e.Error())
+			io.OutputWarning("", e.Error())
 		case error2.CriticalError:
 			io.OutputError(e, "", "")
 			if env.ReadEnv("ENV", "") == "production" {
@@ -56,9 +57,8 @@ func main() {
 	if foundPerson == nil {
 		io.OutputInfo("", "Noone found\n")
 	} else {
-		io.OutputInfo("","Hello '%s', age rate: %d\n", foundPerson.name, foundPerson.ageRate)
+		io.OutputInfo("", "Hello '%s', age rate: %d\n", foundPerson.name, foundPerson.ageRate)
 	}
-
 
 	//
 	//foundPerson = findPerson(persons, "Roman")
@@ -82,15 +82,16 @@ func main() {
 	//	fmt.Printf("Hello '%s', age: %d\n", foundPerson.name, foundPerson.age)
 	//}
 
-	left := 1
-	right := 2
 	factor := 0
-	result := addWithFactor(left, right, &factor)
-
-	for i:=0; i<10; i++ {
-		go addWithFactor(i*1, i*2, &factor)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			addWithFactor(i, i + 1, &factor)
+			wg.Done()
+		}()
 	}
-	io.OutputInfo("", "I had input %d and %d and factor %d and result is %d\n", left, right, factor, result)
+	wg.Wait()
 }
 
 func sendResponseToUser(output string) {
@@ -112,11 +113,11 @@ func findPerson(name string) (person *Person, err error) {
 		}
 	}
 
-	return nil,nil
+	return nil, nil
 }
 
 func getAllPersonsFromDb() (dbPersons []Person, err error) {
-	io.OutputInfo("","Getting users from db")
+	io.OutputInfo("", "Getting users from db")
 	if len(persons) == 0 {
 		err = error2.WarningError{
 			error2.NewErrorWrapper(fmt.Errorf("Database is empty, it has [%d] entries", len(persons))),
@@ -131,7 +132,7 @@ func getAllPersonsFromDb() (dbPersons []Person, err error) {
 		return
 	}
 
-	io.OutputInfo("","Got %d users from db\n", len(persons))
+	io.OutputInfo("", "Got %d users from db\n", len(persons))
 	dbPersons = persons
 	return
 }
@@ -140,10 +141,11 @@ func addWithFactor(left, right int, factorPtr *int) int {
 	internalFactor := *factorPtr
 
 	if internalFactor == 0 {
-		internalFactor = 1
+		internalFactor = left+right
 		*factorPtr = internalFactor
 	}
 
-	return (left + right) * internalFactor
+	res := (left + right) * internalFactor
+	io.OutputInfo("", "I had input %d and %d and factor %d and result is %d\n", left, right, internalFactor, res)
+	return res
 }
-
